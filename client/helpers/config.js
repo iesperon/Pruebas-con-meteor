d2ccc11e96de859896227605dededec5c9fac952
iesperon <ivanesperon@gmail.com>
2016-01-28 17:52:15 +0100

    Usuarios y posts
    -Creacion de usuarios
    -Autenticacion de usuarios
    -Creacion de posts
    -No dar la opcion de crear posts a la gente que no esté autenticada.
    -Meter los datos de usuario, usuarioId y fecha en los post.
    -No dejar crear posts iguales.
    - No dejar crear post con la misma url.

---
+++ client/helpers/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by iesperon on 28/01/2016.
+ */
+//Esto sirve para decirle que solo nos autenticamos con el nombre de usuario
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,5 +3,6 @@
  */
 Template.postsList.helpers({
     posts: function() {
-        return Posts.find();  }
-});
+        //Con el sort llamamos a una funcion de mongo y le decimos que ordene por fecha de subida y el -1 es para el orden
+        return Posts.find({}, {sort: {submitted: -1}});
+    }});

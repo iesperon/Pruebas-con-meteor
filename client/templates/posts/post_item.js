3d4a4cf408775e7036a3939bffe4d9422defe705
iesperon <ivanesperon@gmail.com>
2016-02-02 20:33:23 +0100

    Edicion y eliminacion
    -Permitimos editar los posts.
    - Metemos restricciones para que no los pueda editar nadie que no sea
    propietario del post.
    -Metemos restricciones para que no puedan editar mas información de la
    disponible

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,11 @@
  * Created by iesperon on 26/01/2016.
  */
 Template.postItem.helpers({
+    //Para que solo puedan editar los post la gente que los creo
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
+
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
